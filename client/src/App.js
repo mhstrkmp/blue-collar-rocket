0411f536db9686294a6a4efdc5155cfb16ee355d
Matthias Heisterkamp <hello@mhstrkmp.dev>
2021-01-06 13:17:44 +0100

    :bug: Fix appprovider usage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import AppProviders from "./context/AppProviders";
 import AuthenticatedApp from "./AuthenticatedApp";
 import UnauthenticatedApp from "./UnauthenticatedApp";
 import { useUser } from "./context/UserContext";
 
-function App() {
+export const AuthApp = () => {
   const user = useUser();
   return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+};
+
+function App() {
+  return (
+    <AppProviders>
+      <AuthApp />
+    </AppProviders>
+  );
 }
 
 export default App;
