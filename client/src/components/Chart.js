314ff68872727734ef2e8dc43e73661a84545f43
Matthias Heisterkamp <hello@mhstrkmp.dev>
2021-01-07 22:24:34 +0100

    :construction: Add orders table

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { Bar } from "react-chartjs-2";
+import styled from "styled-components/macro";
+import { isoDateConverter } from "../utils/utils";
+const axios = require("axios").default;
 
-const data = {
+const chartData = {
   labels: [
     "Jan",
     "Feb",
@@ -65,11 +69,62 @@ const options = {
   },
 };
 
-const Chart = () => (
-  <>
-    <h1>Deine Bonuszahlungen</h1>
-    <Bar data={data} options={options} />
-  </>
-);
+const Table = styled.table`
+  margin: 1.8em 0;
+  font-size: 0.8em;
+  th {
+    padding-bottom: 0.6em;
+  }
+  td {
+    padding-bottom: 0.3em;
+  }
+  td:not(:last-child) {
+    padding-right: 0.5em;
+  }
+`;
+
+const Chart = () => {
+  const { isLoading, error, data } = useQuery("orders", async () => {
+    try {
+      const { data } = await axios.get("/api/orders");
+      return data;
+    } catch (err) {
+      // Error handling
+      console.error(err);
+      return err;
+    }
+  });
+
+  if (isLoading) return "Loading...";
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <>
+      <h2>Deine Bonuszahlungen</h2>
+      <Bar data={chartData} options={options} />
+      <Table>
+        <thead>
+          <tr>
+            <th colSpan="4">Vermittelte Aufträge</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={`tr_${index}`}>
+              <td key={`td_date${index}`}>
+                {isoDateConverter(item.dateAdded)}
+              </td>
+              <td key={`td_customer_${index}`}>{item.customer.name}</td>
+              <td key={`td_name_${index}`}>{item.name}</td>
+              <td key={`td_price${index}`}>€{item.price.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
 
 export default Chart;
+
+//{data.customer.map((item) => console.log(item.userId))}
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,3 +1,9 @@
+const isoDateConverter = (isoString) => {
+  const options = { year: "2-digit", month: "2-digit", day: "2-digit" };
+  const date = new Date(isoString);
+  return date.toLocaleDateString("de-DE", options);
+};
+
 const makeExerp = (text, length) => {
   if (text.length <= length - 3) {
     return text;
@@ -14,5 +20,6 @@ const getLocalStorageCart = (cartId) => {
   return cart;
 };
 
+exports.isoDateConverter = isoDateConverter;
 exports.getLocalStorageCart = getLocalStorageCart;
 exports.makeExerp = makeExerp;
